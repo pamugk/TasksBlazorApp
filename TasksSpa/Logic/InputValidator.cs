d5559c1c6c68c99d9e211e100480daf6fa5c08ae
Alexander <pamugk@gmail.com>
2019-11-11 21:22:36 +0500

    Razor app components implementation (except for a few); App data classes (task and user)

---
+++ TasksSpa/Logic/InputValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace TasksSpa.Logic
+{
+    public static class InputValidator
+    {
+        public static bool LoginIsValid(string login)
+        {
+            return login != null && login.Trim().Length > 0 && !login.Contains("@");
+        }
+
+        public static bool PasswordIsValid(string password)
+        {
+            return password != null && password.Trim().Length > 0;
+        }
+    }
+}
---
+++ TasksSpa/Model/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TasksSpa.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsFinished{ get; set; }
+    }
+}
