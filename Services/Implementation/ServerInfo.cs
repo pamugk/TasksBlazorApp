a86d4f15b4978441ea5131324ecf5f74da2a2a4e
Alexander Permyakov <pamugk@gmail.com>
2020-07-05 19:02:19 +0500

    Changed remote server url

--- Services/Implementation/ServerInfo.cs
+++ Services/Implementation/ServerInfo.cs
@@ -2,7 +2,7 @@ namespace TodosSpa.Services.Implementation
 {
     static class ServerInfo
     {
-        public const string Url = "https://todos-rest-api-server.herokuapp.com";
+        public const string Url = "https://flask-todos-rest-api.herokuapp.com/";
         //public const string Url = "http://localhost:5000";
     }
 }
